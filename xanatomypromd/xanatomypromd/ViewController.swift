a560629cbbf69f95661459c3e5cca47e268c5440
Francis Isidoro <francis@isidoro.net>
2025-07-16 10:00:20 -0700

    Initial X-Code project setup for XAnatomyPROMD

---
+++ xanatomypromd/xanatomypromd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xanatomypromd
+//
+//  Created by fpisidoro on 7/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ xanatomypromd/xanatomypromdTests/xanatomypromdTests.swift
@@ -0,0 +1,17 @@
+//
+//  xanatomypromdTests.swift
+//  xanatomypromdTests
+//
+//  Created by fpisidoro on 7/16/25.
+//
+
+import Testing
+@testable import xanatomypromd
+
+struct xanatomypromdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
